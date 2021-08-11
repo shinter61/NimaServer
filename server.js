a0716c4abec4605aa266435312322fdc4e8d909c
shinter61 <shinter61@gmail.com>
2021-08-11 13:21:24 +0900

    Sync discards

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 connections = [];
+class Player {
+  constructor() {
+    this.name = ""
+    this.discards = ""
+  }
+}
 class Game {
   constructor() {
-    this.player1 = ""
-    this.player2 = ""
+    this.player1 = new Player()
+    this.player2 = new Player()
   }
 }
 var game = new Game();
@@ -22,28 +28,37 @@ io.sockets.on('connection', function(socket) {
   // Disconnect
   socket.on('disconnect', function(data) {
     let playerID = socket.handshake.auth.name
-    if (game.player1 == playerID) {
-      game.player1 = ""
-    } else if (game.player2 == playerID) {
-      game.player2 = ""
+    if (game.player1.name == playerID) {
+      game.player1 = new Player()
+    } else if (game.player2.name == playerID) {
+      game.player2 = new Player()
     }
-    console.log("player1 ", game.player1)
-    console.log("player2 ", game.player2)
-    io.sockets.emit('InformPlayersNames', { player1: game.player1, player2: game.player2 })
+    console.log("player1 ", game.player1.name)
+    console.log("player2 ", game.player2.name)
+    io.sockets.emit('InformPlayersNames', { player1: game.player1.name, player2: game.player2.name })
 
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnect: %s sockets are connected', connections.length);
   });
 
   socket.on('AddPlayer', function(data) {
-    if (game.player1 == "") {
-      game.player1 = data
-    } else if (game.player2 == "") {
-      game.player2 = data
+    if (game.player1.name == "") {
+      game.player1.name = data
+    } else if (game.player2.name == "") {
+      game.player2.name = data
     }
-    console.log("player1 ", game.player1)
-    console.log("player2 ", game.player2)
-    io.sockets.emit('InformPlayersNames', { player1: game.player1, player2: game.player2 })
+    console.log("player1 ", game.player1.name)
+    console.log("player2 ", game.player2.name)
+    io.sockets.emit('InformPlayersNames', { player1: game.player1.name, player2: game.player2.name })
+  })
+
+  socket.on('Discard', function(playerID, discards) {
+    if (game.player1.name == playerID) {
+      game.player1.discards = discards
+    } else if (game.player2.name == playerID) {
+      game.player2.discards = discards
+    }
+    io.sockets.emit('InformDiscards', { id: playerID, discards: discards })
   })
 })
 
