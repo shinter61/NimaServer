55deca61e97df5fd0eebb9f1835cf63104dee57a
shinter61 <shinter61@gmail.com>
2021-11-12 09:12:58 +0900

    Fix test

--- tests/waitTiles.test.ts
+++ tests/waitTiles.test.ts
@@ -1,8 +1,10 @@
 import { Tile } from "../src/Tile"
 import { Player } from "../src/Player"
+import { Game } from "../src/Game"
 
 describe("3面待ちテスト", () => {
   test("3面待ちの例1が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""),
@@ -11,11 +13,12 @@ describe("3面待ちテスト", () => {
       new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 9, ""),
       new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 1, ""), new Tile("sou", 4, ""), new Tile("sou", 7, "")])
   })
 
   test("3面待ちの例2の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 3, ""),
@@ -24,11 +27,12 @@ describe("3面待ちテスト", () => {
       new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 9, ""),
       new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 1, ""), new Tile("sou", 4, ""), new Tile("man", 1, "")])
   })
 
   test("3面待ちの例3の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""),
@@ -37,11 +41,12 @@ describe("3面待ちテスト", () => {
       new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 9, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 5, "")])
   })
 
   test("3面待ちの例4の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 1, ""),
@@ -50,11 +55,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 1, ""), new Tile("sou", 4, ""), new Tile("sou", 7, "")])
   })
 
   test("3面待ちの例5の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""),
@@ -63,11 +69,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 5, ""), new Tile("sou", 8, "")])
   })
 
   test("3面待ちの例6の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""), 
@@ -75,11 +82,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 8, "")])
   })
 
   test("3面待ちの例7の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""), 
@@ -87,11 +95,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 7, "")])
   })
 
   test("3面待ちの例8の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 2, ""), 
@@ -99,11 +108,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 3, ""), new Tile("sou", 4, ""), new Tile("sou", 5, "")])
   })
 
   test("3面待ちの例9の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""), 
@@ -111,11 +121,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 4, "")])
   })
 
   test("3面待ちの例10の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""), 
@@ -123,11 +134,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 5, "")])
   })
 
   test("3面待ちの例11の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -135,11 +147,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 5, ""), new Tile("pin", 1, "")])
   })
 
   test("3面待ちの例12の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -147,11 +160,12 @@ describe("3面待ちテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 5, ""), new Tile("pin", 1, "")])
   })
 
   test("3面待ちの例13の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 1, ""), new Tile("sou", 1, ""), new Tile("sou", 1, ""), 
@@ -159,13 +173,14 @@ describe("3面待ちテスト", () => {
       new Tile("sou", 6, ""), new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 9, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 9, "")])
   })
 })
 
 describe("4面待ちのテスト", () => {
   test("4面待ちの例1の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 4, ""), new Tile("sou", 4, ""), 
@@ -173,11 +188,12 @@ describe("4面待ちのテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 5, ""), new Tile("sou", 8, "")])
   })
 
   test("4面待ちの例2の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 4, ""), new Tile("sou", 5, ""), 
@@ -185,11 +201,12 @@ describe("4面待ちのテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 7, "")])
   })
 
   test("4面待ちの例3の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""), new Tile("sou", 2, ""), 
@@ -197,11 +214,12 @@ describe("4面待ちのテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 4, ""), new Tile("sou", 5, "")])
   })
 
   test("4面待ちの例4の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 2, ""),
@@ -209,11 +227,12 @@ describe("4面待ちのテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 1, ""), new Tile("sou", 2, ""), new Tile("sou", 4, ""), new Tile("sou", 5, "")])
   })
 
   test("4面待ちの例5の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -221,11 +240,12 @@ describe("4面待ちのテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 3, ""), new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 6, "")])
   })
 
   test("4面待ちの例6の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 1, ""), new Tile("sou", 1, ""), new Tile("sou", 1, ""),
@@ -233,13 +253,14 @@ describe("4面待ちのテスト", () => {
       new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 9, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 6, ""), new Tile("sou", 9, "")])
   })
 })
 
 describe("5面待ち以上のテスト", () => {
   test("5面待ちの例1の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -247,13 +268,14 @@ describe("5面待ち以上のテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 2, ""), new Tile("sou", 4, ""), new Tile("sou", 5, ""),
       new Tile("sou", 7, ""), new Tile("sou", 8, "")])
   })
 
   test("5面待ちの例2の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 3, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -261,13 +283,14 @@ describe("5面待ち以上のテスト", () => {
       new Tile("pin", 1, ""), new Tile("pin", 1, ""), new Tile("pin", 1, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 4, ""),
       new Tile("sou", 5, ""), new Tile("sou", 6, "")])
   })
 
   test("6面待ちの例1の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 4, ""),
@@ -275,13 +298,14 @@ describe("5面待ち以上のテスト", () => {
       new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 7, ""), new Tile("sou", 8, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 5, ""),
       new Tile("sou", 6, ""), new Tile("sou", 8, ""), new Tile("sou", 9, "")])
   })
 
   test("6面待ちの例2の待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 4, ""),
@@ -289,13 +313,14 @@ describe("5面待ち以上のテスト", () => {
       new Tile("sou", 7, ""), new Tile("sou", 8, ""), new Tile("sou", 8, ""), new Tile("sou", 8, ""),
       new Tile("man", 1, ""), new Tile("man", 1, ""), new Tile("man", 1, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 1, ""), new Tile("sou", 3, ""), new Tile("sou", 4, ""),
       new Tile("sou", 6, ""), new Tile("sou", 7, ""), new Tile("sou", 9, "")])
   })
 
   test("7面待ちの待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -303,13 +328,14 @@ describe("5面待ち以上のテスト", () => {
       new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 7, ""), new Tile("sou", 7, ""),
       new Tile("sou", 7, ""), new Tile("sou", 7, ""), new Tile("sou", 8, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 1, ""), new Tile("sou", 2, ""), new Tile("sou", 4, ""),
       new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 8, ""), new Tile("sou", 9, "")])
   })
 
   test("8面待ちの待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 2, ""), new Tile("sou", 3, ""), new Tile("sou", 3, ""),
@@ -317,7 +343,7 @@ describe("5面待ち以上のテスト", () => {
       new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 7, ""),
       new Tile("sou", 8, ""), new Tile("sou", 8, ""), new Tile("sou", 8, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     // 3pは5枚目を使用することになるので待ちに含まれないが、5枚目なら弾く処理は面倒なので実装しない
     expect(waits).toEqual([
       new Tile("sou", 1, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""),
@@ -326,6 +352,7 @@ describe("5面待ち以上のテスト", () => {
   })
 
   test("9面待ちの待ち牌が計算できる", () => {
+    const game = new Game;
     const testPlayer = new Player()
     testPlayer.tiles = [
       new Tile("sou", 1, ""), new Tile("sou", 1, ""), new Tile("sou", 1, ""),
@@ -333,7 +360,7 @@ describe("5面待ち以上のテスト", () => {
       new Tile("sou", 5, ""), new Tile("sou", 6, ""), new Tile("sou", 7, ""), new Tile("sou", 8, ""),
       new Tile("sou", 9, ""), new Tile("sou", 9, ""), new Tile("sou", 9, "")
     ]
-    const waits: Tile[] = testPlayer.waitTiles()[0]
+    const waits: Tile[] = testPlayer.waitTiles(game)[0]
     expect(waits).toEqual([
       new Tile("sou", 1, ""), new Tile("sou", 2, ""), new Tile("sou", 3, ""),
       new Tile("sou", 4, ""), new Tile("sou", 5, ""), new Tile("sou", 6, ""),
@@ -342,6 +369,7 @@ describe("5面待ち以上のテスト", () => {
 })
 
 test("七対子の待ち牌が計算できる", () => {
+  const game = new Game;
   const testPlayer = new Player()
   testPlayer.tiles = [
     new Tile("", 0, "white"), new Tile("", 0, "white"),
@@ -352,11 +380,12 @@ test("七対子の待ち牌が計算できる", () => {
     new Tile("sou", 5, ""), new Tile("sou", 5, ""),
     new Tile("sou", 6, ""), new Tile("sou", 6, ""),
   ]
-  const waits: Tile[] = testPlayer.waitTiles()[0]
+  const waits: Tile[] = testPlayer.waitTiles(game)[0]
   expect(waits).toEqual([new Tile("sou", 1, "")])
 })
 
 test("国士無双の待ち牌が計算できる", () => {
+  const game = new Game;
   const testPlayer = new Player()
   testPlayer.tiles = [
     new Tile("sou", 1, ""), new Tile("sou", 9, ""), new Tile("pin", 1, ""),
@@ -364,11 +393,12 @@ test("国士無双の待ち牌が計算できる", () => {
     new Tile("", 0, "east"), new Tile("", 0, "south"), new Tile("", 0, "west"),
     new Tile("", 0, "west"), new Tile("", 0, "north"), new Tile("", 0, "white"), new Tile("", 0, "green")
   ]
-  const waits: Tile[] = testPlayer.waitTiles()[0]
+  const waits: Tile[] = testPlayer.waitTiles(game)[0]
   expect(waits).toEqual([new Tile("", 0, "red")])
 })
 
 test("国士無双十三面の待ち牌が計算できる", () => {
+  const game = new Game;
   const testPlayer = new Player()
   testPlayer.tiles = [
     new Tile("sou", 1, ""), new Tile("sou", 9, ""), new Tile("pin", 1, ""),
@@ -376,7 +406,7 @@ test("国士無双十三面の待ち牌が計算できる", () => {
     new Tile("", 0, "east"), new Tile("", 0, "south"), new Tile("", 0, "west"), new Tile("", 0, "north"),
     new Tile("", 0, "white"), new Tile("", 0, "green"), new Tile("", 0, "red")
   ]
-  const waits: Tile[] = testPlayer.waitTiles()[0]
+  const waits: Tile[] = testPlayer.waitTiles(game)[0]
   expect(waits).toEqual([
     new Tile("sou", 1, ""), new Tile("sou", 9, ""), new Tile("pin", 1, ""),
     new Tile("pin", 9, ""), new Tile("man", 1, ""), new Tile("man", 9, ""),
