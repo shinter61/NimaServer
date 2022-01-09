459df2fa52398bdd59de065bd1a0bad5d150e90b
shinter61 <shinter61@gmail.com>
2022-01-09 17:20:43 +0900

    Fix handling riichi discard tile

--- src/server.ts
+++ src/server.ts
@@ -169,10 +169,8 @@ io.sockets.on('connection', function(socket: Socket) {
     game.player1.id === Number(playerID) ? rooms[roomID].player1 = player : rooms[roomID].player2 = player
   })
 
-  socket.on('InformRiichi', function(roomID: string, playerID: string, tiles: string) {
-    const tileObj = (JSON.parse(tiles) as Tile[])[0]
-    const discardTile = new Tile(tileObj.kind, tileObj.number, tileObj.character)
-    io.to(roomID).emit('InformRiichi', { id: String(playerID), discardTile: JSON.stringify([discardTile]) })
+  socket.on('InformRiichi', function(roomID: string, playerID: string) {
+    io.to(roomID).emit('InformRiichi', { id: String(playerID) })
   })
 
   socket.on('InformPon', function(roomID: string, playerID: string) {
